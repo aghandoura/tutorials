b43dc4e413b8bf2789946b903a9b9f5f8d2fffa8
Adam Ghandoura <adam.ghandoura@st.com>
2016-08-23 11:05:37 +0100

    use uniform distribution
    Change-Id: I12d9cfd82999293a666140a414e6a7431c9f3196

--- multithreading/merge_sort.c
+++ multithreading/merge_sort.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define N 2  /* # of thread */
+#define N 2 /* # of thread */
+/////!!!!!!!threads are created recursively keep this low
+#define MAX_ELEM 100  /* # of thread */
 
-int a[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};  /* target array */
+int *a;  /* target array */
+pthread_mutex_t mutex;
 
 /* structure for array index
  * used to keep low/high end of sub arrays
@@ -14,9 +17,23 @@ typedef struct Arr {
     int high;
 } ArrayIndex;
 
+int uniform_distribution(int rangeLow, int rangeHigh)
+{
+    int range = rangeHigh - rangeLow + 1; //+1 makes it [rangeLow, rangeHigh], inclusive.
+    int copies=RAND_MAX/range; // we can fit n-copies of [0...range-1] into RAND_MAX
+    // Use rejection sampling to avoid distribution errors
+    int limit=range*copies;
+    int myRand=-1;
+    while( myRand<0 || myRand>=limit){
+        myRand=rand();
+    }
+    return myRand/copies+rangeLow;    // note that this involves the high-bits
+}
+
 void merge(int low, int high)
 {
-        int mid = (low+high)/2;
+        /*pthread_mutex_lock(&mutex);*/
+        int mid = low + (high - low)/2;
         int left = low;
         int right = mid+1;
 
@@ -27,18 +44,20 @@ void merge(int low, int high)
                 if (a[left] > a[right])
                         b[cur++] = a[right++];
                 else
-                        b[cur++] = a[right++];
+                        b[cur++] = a[left++];
         }
 
         while(left <= mid) b[cur++] = a[left++];
-        while(right <= high) b[cur++] = a[left++];
+        while(right <= high) b[cur++] = a[right++];
         for (i = 0; i < (high-low+1) ; i++) a[low+i] = b[i];
+
+        /*pthread_mutex_unlock(&mutex);*/
 }
 
 void * mergesort(void *a)
 {
         ArrayIndex *pa = (ArrayIndex *)a;
-        int mid = (pa->low + pa->high)/2;
+        int mid = pa->low + (pa->high - pa->low)/2;
 
         ArrayIndex aIndex[N];
         pthread_t thread[N];
@@ -49,7 +68,7 @@ void * mergesort(void *a)
         aIndex[1].low = mid+1;
         aIndex[1].high = pa->high;
 
-        if (pa->low >= pa->high) return;
+        if (pa->low >= pa->high) return NULL;
 
         int i;
         for(i = 0; i < N; i++) pthread_create(&thread[i], NULL, mergesort, &aIndex[i]);
@@ -63,15 +82,44 @@ void * mergesort(void *a)
 int main()
 {
         ArrayIndex ai;
+        int index = 0;
+        int i;
+        time_t seconds;
+
+        seconds = time(NULL);
+
+        time(&seconds);
+
+        srand ((unsigned) seconds);
+
+        a = malloc( MAX_ELEM * sizeof(int));
+        if (!a)
+        {
+            printf("malloc failed \n");
+            return -1;
+        }
+
+        for (index = 0; index < MAX_ELEM; index++)
+        {
+            a[index] = uniform_distribution(0, index);
+
+        }
+
+        for (i = 0; i < MAX_ELEM; i++) printf ("%d ", a[i]);
+        printf("\n");
+
         ai.low = 0;
-        ai.high = sizeof(a)/sizeof(a[0])-1;
+        ai.high = MAX_ELEM;
         pthread_t thread;
 
         pthread_create(&thread, NULL, mergesort, &ai);
         pthread_join(thread, NULL);
 
-        int i;
-        for (i = 0; i < 10; i++) printf ("%d ", a[i]);
+        for (i = 0; i < MAX_ELEM; i++) printf ("%d ", a[i]);
+        printf("\n");
+
+        pthread_mutex_destroy(&mutex);
+        free(a);
 
         return 0;
 }
